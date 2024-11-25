eb946b7f64f5bbae9b79274457e93ee4909bb671
ian dorian <doryan.ian@gmail.com>
2024-11-24 18:36:11 -0600

    agrugue la funcion de ClonaPalabras a la main

--- Corrector.cpp
+++ Corrector.cpp
@@ -143,7 +143,8 @@ void	ListaCandidatas		(
 	int		iNumElementos,						//Numero de elementos en el diccionario
 	char	szListaFinal[][TAMTOKEN],			//Lista final de palabras a sugerir
 	int		iPeso[],							//Peso de las palabras en la lista final
-	int &	iNumLista)							//Numero de elementos en la szListaFinal
+	int &	iNumLista)//Numero de elementos en la szListaFinal
+
 
 /*****************************************************************************************************************
 	ClonaPalabras: toma una palabra y obtiene todas las combinaciones y permutaciones requeridas por el metodo
@@ -154,4 +155,83 @@ void	ListaCandidatas		(
 void	ClonaPalabras(
 	char *	szPalabraLeida,						// Palabra a clonar
 	char	szPalabrasSugeridas[][TAMTOKEN], 	//Lista de palabras clonadas
-	int &	iNumSugeridas)						//Numero de elementos en la lista
+	int &	iNumSugeridas)	{					//Numero de elementos en la lista
+    char copia[TAMTOKEN] = { 0 };
+char ALFABETO[] = "abcdefghijklmnñopqrstuvwxyzáéíóú";
+int lenPalabraLeida = strlen(szPalabraLeida);
+int lenAlfabeto = strlen(ALFABETO);
+iNumSugeridas = 0;
+//palabra original a la lista de sugerencias
+strcpy_s(szPalabrasSugeridas[iNumSugeridas], TAMTOKEN, szPalabraLeida);
+iNumSugeridas++;
+
+//sustitucion de caracteres
+for (int i = 0; i < lenPalabraLeida; i++) {
+    for (int j = 0; j < lenAlfabeto; j++) {
+        strncpy_s(copia, TAMTOKEN, szPalabraLeida, TAMTOKEN - 1);
+        copia[i] = ALFABETO[j];
+        if (iNumSugeridas < 3300) {
+            strcpy_s(szPalabrasSugeridas[iNumSugeridas], TAMTOKEN, copia);
+            //printf("Añadido: %s (iNumSugeridas: %d)\n", copia, iNumSugeridas + 1);
+            iNumSugeridas++;
+        }
+    }
+}
+
+//se ponen los caracteres en los posibles espacios, pero solo si hay uno o mas caractres en la cadena original
+if (lenPalabraLeida >= 1) {
+    for (int i = 0; i <= lenPalabraLeida; i++) {
+        for (int j = 0; j < lenAlfabeto; j++) {
+            strncpy_s(copia, TAMTOKEN, szPalabraLeida, i);
+            copia[i] = ALFABETO[j];
+            strncpy_s(copia + i + 1, TAMTOKEN - i - 1, szPalabraLeida + i, TAMTOKEN - i - 1);
+            copia[lenPalabraLeida + 1] = '\0';
+            if (iNumSugeridas < 3300) {
+                strcpy_s(szPalabrasSugeridas[iNumSugeridas], TAMTOKEN, copia);
+                //printf("Añadido: %s (iNumSugeridas: %d)\n", copia, iNumSugeridas + 1);
+                iNumSugeridas++;
+            }
+        }
+    }
+}
+
+//eliminacion de caracteres pero solo si hay mas de uno
+if (lenPalabraLeida > 1) {
+    for (int i = 0; i < lenPalabraLeida; i++) {
+        strncpy_s(copia, TAMTOKEN, szPalabraLeida, i);
+        strncpy_s(copia + i, TAMTOKEN - i, szPalabraLeida + i + 1, TAMTOKEN - i - 1);
+        copia[lenPalabraLeida] = '\0';
+        if (iNumSugeridas < 3300) {
+            strcpy_s(szPalabrasSugeridas[iNumSugeridas], TAMTOKEN, copia);
+            //printf("Añadido: %s (iNumSugeridas: %d)\n", copia, iNumSugeridas + 1);
+            iNumSugeridas++;
+        }
+    }
+    // intercambio de caracteres solo si hay mas de uno  
+    for (int i = 0; i < lenPalabraLeida - 1; i++) {// i hasta len-1
+        for (int j = i + 1; j < i + 2; j++) {  // Solo un intercambio entre i y i+1
+            // Crear una copia de la palabra original
+            strcpy_s(copia, TAMTOKEN, szPalabraLeida);
+            char temp = copia[i];
+            copia[i] = copia[i + 1];
+            copia[i + 1] = temp;
+            if (iNumSugeridas < 3300) {
+                strcpy_s(szPalabrasSugeridas[iNumSugeridas], TAMTOKEN, copia);
+                //printf("Añadido: %s (iNumSugeridas: %d)\n", copia, iNumSugeridas + 1);
+                iNumSugeridas++;
+            }
+        }
+    }
+}
+//ordencion de las sugerencias de manera alfabetica B)
+char temporal[TAMTOKEN];
+for (int a = 0;a < iNumSugeridas - 1; a++) {
+    for (int b = 0;b < iNumSugeridas - 1 - a;b++) {
+        if (strcmp(szPalabrasSugeridas[b], szPalabrasSugeridas[b + 1]) > 0) {
+            strcpy_s(temporal, szPalabrasSugeridas[b]);
+            strcpy_s(szPalabrasSugeridas[b], szPalabrasSugeridas[b + 1]);
+            strcpy_s(szPalabrasSugeridas[b + 1], temporal);
+        }
+    }
+}
+}
